721ca00d6fd4c97b7bd3ec94efe22c712f07506d
PabloHerrera99 <p.herrera@alumno.um.edu.ar>
2023-04-04 11:42:23 -0300

    clase2

---
+++ Ejercicios/Clase2/ejercicio4.py
@@ -0,0 +1,26 @@
+import argparse
+import os
+import cmath
+
+def fraiz():
+    parser = argparse.ArgumentParser(description="calcular raiz")
+    parser.add_argument("-n", "--number", type=int, help='debe ser un número')
+    parser.add_argument("-f", "--fork", action="store true", help="calcula la raiz negativa")
+    
+    args = parser.parse_args()
+    
+    r = cmath.sqrt(args.number)
+
+    if args.fork:
+        rt = os.fork()
+        
+        if rt > 0:
+            print(f'La raiz positiva es{r}')
+        
+        elif rt == 0:
+            print(f'la raiz negativa es{-r}')
+    if not args.fork:
+        print(f'La raiz positiva es{r}')
+
+fraiz()
+
